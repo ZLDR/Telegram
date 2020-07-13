713c051ef05fab5236f9c1919e98c6f81e494c13
Dorian Zlatan <zlatanair@gmail.com>
2020-07-14 02:08:43 +0300

    first commit

---
+++ Telegram/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Telegram
+//
+//  Created by Dorian ZLATAN on 14/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, worldd!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Telegram/TelegramApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelegramApp.swift
+//  Telegram
+//
+//  Created by Dorian ZLATAN on 14/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct TelegramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
