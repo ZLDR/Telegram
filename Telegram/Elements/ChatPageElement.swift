f65abb1732f7bf8eb18d0e87848747efd5af6633
Dorian Zlatan <dorian@hey.com>
2021-03-31 18:38:44 +0300

    Trying to make it work

--- Telegram/Elements/ChatPageElement.swift
+++ Telegram/Elements/ChatPageElement.swift
@@ -16,7 +16,6 @@ struct ChatPageElement: View {
 			
 			ForEach(Service.listData){ item in
 				VStack{
-					
 					HStack(alignment: .center){
 						Image(item.photo)
 							.resizable()
@@ -24,21 +23,15 @@ struct ChatPageElement: View {
 							.clipShape(Circle())
 							.frame(width: 50, height: 50, alignment: .leading)
 							.padding(.leading)
-						
 						VStack(alignment: .leading){
-							
 							HStack(alignment: .top) {
-								
 								Text(item.name)
 									.font(.system(size: 16)).bold()
 									.foregroundColor(.white)
 									.padding(.top, 10)
-								
 								Spacer()
-								
 								Image(item.status).imageScale(.small)
 									.padding(.top, 10)
-								
 								Text(item.time)
 									.font(.subheadline)
 									.padding(.leading, -5)
@@ -47,20 +40,15 @@ struct ChatPageElement: View {
 									.padding(.trailing)
 							}
 							.padding(.top, 5)
-							
 							HStack{
-								
 								HStack{
-									
 									Text(item.text)
 										.font(.system(size: 14))
 										.foregroundColor(.gray)
 										.frame(width: 260, height: 50, alignment: .topLeading)
 										.lineLimit(2)
 										.padding(.top, -10)
-									
 									Spacer()
-									
 									Image(systemName: item.pinned)
 										.rotationEffect(.init(degrees: 45))
 										.foregroundColor(.gray)
--- Telegram/Elements/PinChatPageElement.swift
+++ Telegram/Elements/PinChatPageElement.swift
@@ -8,59 +8,50 @@
 import SwiftUI
 
 struct PinChatPageElement: View {
-	var body: some View {
-		VStack(alignment: .trailing ,spacing: 0){
-			ForEach(Service2.listData){ item in
-				VStack(alignment: .trailing, spacing: 0){
-					HStack{
-						Image(item.photo)
-							.resizable()
-							.aspectRatio(contentMode: .fill)
-							.clipShape(Circle())
-							.frame(width: 50, height: 50, alignment: .leading)
-							.padding(.leading)
-						VStack(alignment: .leading){
-							HStack(alignment: .center) {
-								Text(item.name)
-									.font(.system(size: 16)).bold()
-									.foregroundColor(.white)
-									.padding(.top, 10)
-								Spacer()
-								Image(item.status).imageScale(.small)
-								Text(item.time)
-									.font(.subheadline)
-									.padding(.leading, -5)
-									.foregroundColor(.gray)
-									.padding(.trailing)
-							}
-							.padding(.top, 5)
-							HStack(alignment: .top){
-								HStack(alignment: .center) {
-									Text(item.text)
-										.font(.system(size: 14))
-										.foregroundColor(.gray)
-										.frame(width: 260, height: 50, alignment: .topLeading)
-										.padding(.top, -10)
-
-									Spacer()
-									Image(systemName: item.pinned)
-										.rotationEffect(.init(degrees: 45))
-										.foregroundColor(.gray)
-										.padding(.trailing)
-								}
-							}
-						}
-					}
-					.background(Color(UIColor.secondarySystemBackground))
-				}
-			}
-		}
-	}
+    var body: some View {
+        VStack(spacing: 0){
+            ForEach(Service2.listData){ item in
+                VStack{
+                    HStack{
+                        Image(item.photo)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .clipShape(Circle())
+                            .frame(width: 50, height: 50, alignment: .leading)
+                        VStack{
+                            HStack{
+                                Text(item.name)
+                                    .font(.system(size: 16)).bold()
+                                    .foregroundColor(.white)
+                                Spacer()
+                                Image(item.status).imageScale(.small)
+                                Text(item.time)
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
+                            HStack{
+                                Text(item.text)
+                                    .font(.system(size: 14))
+                                    .foregroundColor(.gray)
+                                    .frame(width: 260, height: 50, alignment: .topLeading)
+                                
+                                Spacer()
+                                Image(systemName: item.pinned)
+                                    .rotationEffect(.init(degrees: 45))
+                                    .foregroundColor(.gray)
+                            }
+                        }
+                    }
+                    .background(Color(UIColor.secondarySystemBackground))
+                }
+            }
+        }
+    }
 }
 
 struct PinChatPageElement_Previews: PreviewProvider {
-	static var previews: some View {
-		PinChatPageElement()
-			.environment(\.colorScheme, .dark)
-	}
+    static var previews: some View {
+        PinChatPageElement()
+            .environment(\.colorScheme, .dark)
+    }
 }
